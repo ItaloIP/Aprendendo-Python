01559409d3260cdcefbf3f3d6b0f8456c9da0b58
Italo Pereira Vieira <mortybostinha@gmail.com>
2022-07-24 11:53:48 -0300

    Criado 11 desafios

--- cursoemvideo/pratica2.py
+++ cursoemvideo/pratica2.py
@@ -1,10 +1,2 @@
-nome = str(input('Qual o seu nome?: ')).strip().capitalize()
-if nome == 'Italo':
-    print('Que nome bonito!')
-elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
-    print('Seu nome é bem popular no Brasil!')
-elif nome in 'Ana Claúdia Jéssica Juliana':
-    print('Belo nome feminino')    
-else:
-    print('Seu nome é bem normal!')
-print('Olá, {}!'.format(nome))
+for c in range(0,10):
+    n = int(input('ddddd'))
