f67fa5d6ba28e03b9fe61fe8bcf8b22e01be41ae
Italo Pereira Vieira <mortybostinha@gmail.com>
2022-07-24 11:14:03 -0300

    feito o exercício 44 e 45

---
+++ cursoemvideo/desafios/desafio44.py
@@ -0,0 +1,33 @@
+from time import sleep
+print('\033[0;32;31mLojas Galaia\33[m')
+
+valor = float(input('Valor em R$ da compra?: R$'))
+
+escolha = int(input('''Você deseja pagar:
+( 1 ) Á vista dinheho ou cheque (10% de desconto)
+( 2 ) Á vista no cartão (5% de desconto)
+( 3 ) 2x no cartão
+( 4 ) 3x ou mais no cartão (20% de juros)
+: '''))
+
+if escolha == 1:
+    pay = valor * 0.90
+    print('Ao pagar a vista com dinheiro, será aplicado 10% de desconto, então valor total será de R${:.2f}'.format(pay))
+elif escolha == 2:
+    pay = valor * 0.95
+    print('Ao pagar a vista com cartão, será aplicado o desconto de 5%. O novo valor será de R${:.2f}'.format(pay))
+elif escolha == 3:
+    pay = valor / 2
+    print('O valor será de R${:.2f} em 2x sem JUROS.'.format(pay))
+elif escolha == 4:
+    div = int(input('Dividir em quantas vezes?: '))
+    if div >= 3:
+        pay = (valor + (valor * 0.20)) / div 
+        print('Você escolheu pagar em {}x, terá um acréscimo de 20% de juros.'.format(div))
+        print('Carregando...')
+        sleep (2)
+        print('Ao total, você pagará R${:.2f} em {}x'.format(pay,div))
+    else:
+        print('Você não pode dividir em {}x, apenas 3x pra cima!')
+else:
+    print('Opção inválida.')
---
+++ cursoemvideo/desafios/desafio45.py
@@ -0,0 +1,49 @@
+from random import randint
+from time import sleep
+itens = 'PEDRA', 'PAPEL', 'TESOURA'
+
+print('Jokenpô!')
+
+robo = int(randint(0,2))
+escolha = int(input(''' Escolha : 
+( 0 ) PEDRA
+( 1 ) PAPEL
+( 2 ) TESOURA
+: '''))
+
+print('-='*8)
+print('Você JOGOU {}'.format(itens[escolha]))
+print('O ROBO jogou {}.'.format(itens[robo]))
+print('-='*8)
+
+sleep(0.8)
+
+if robo == 0: #ROBO joga PEDRA
+    if escolha == 0: #JOGADOR joga PEDRA
+        print('EMPATE')
+    elif escolha == 1: #JOGADOR joga PAPEL
+        print('você GANHOU')
+    elif escolha == 2: #JOGADOR joga TESOURA
+        print('ROBO GANHOU')
+    else: #JOGADOR escolheu algo INVÁLIDO
+        print('JOGADA INVÁLIDA!')
+
+elif robo == 1: #ROBO joga PAPEL
+    if escolha == 0: #JOGADOR joga PEDRA
+        print('você GANHOU!')
+    elif escolha == 1: #JOGADOR joga PAPEL
+        print('EMPATE!')
+    elif escolha == 2: #JOGADOR joga TESOURA
+        print('ROBO GANHOU!')
+    else: #JOGADOR escolheu algo INVÁLIDO
+        print('Jogada inválida')
+
+elif robo == 2: #ROBO joga TESOURA
+    if escolha == 0: #JOGADOR joga PEDRA
+        print('você GANHOU!')
+    elif escolha == 1: #JOGADOR joga PAPEL
+        print('ROBO GANHOU!')
+    elif escolha == 2: #JOGADOR joga TESOURA
+        print('EMPATE!')
+    else: #JOGADOR escolhe algo INVÁLIDO
+        print('JOGADA INVÁLIDA!')
