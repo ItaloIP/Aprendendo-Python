c5e481c9fe2a972e688a4ceb0b298ae0ea328049
Italo Pereira Vieira <mortybostinha@gmail.com>
2022-07-30 19:21:55 -0300

    Realização de todos os exercícios (até o 56)

--- cursoemvideo/desafios/desafio56.py
+++ cursoemvideo/desafios/desafio56.py
@@ -1,28 +1,23 @@
-idade = 0
-media = 0
+somaidade = 0
+mediaidade = 0
 maiorhomem = 0
 nomevelho = ''
 totmulher20 = 0
-for pessoa in range(1, 5):
-    print('---{}Pessoa---'.format(pessoa))
-    nome = str(input('Nome: '))
-    id = int(input('Idade: '))
-    s = str(input('Sexo: F ou M: ')).lower()
-    
-    idade += id
-    
-    if pessoa == 1 and s =='m':
-        maiorhomem = id
-        nomevelho = nome 
-    
-    if s == 'm' and id > maiorhomem:
-        maiorhomem = id
+for p in range(1,5):
+    print('======{} Pessoa ========'.format(p))
+    nome = str(input('Nome: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo F/M: ')).strip()
+    somaidade += idade
+    if p == 1 and sexo in 'Mm':
+        maiorhomem = idade
+        nomevelho - nome
+    if sexo in 'Mm' and idade > maiorhomem:
+        maiorhomem = idade
         nomevelho = nome
-    
-    if s in 'f' and id < 20:
+    if sexo in 'Ff' and idade < 20:
         totmulher20 += 1
-
-media = idade / 4
-print('A média do grupo é de {} anos.'.format(media))
-print('O homem mais velho é o {} com {} anos.'.format(nome,idade))
-print('Há {} mulher(es) com menos de 20 anos.'.format(totmulher20))
+mediaidade = somaidade / 4
+print('A média de idade destas pessoas é de {} anos.'.format(mediaidade))
+print('O homem mais velho se chama {} e tem {} anos.'.format(nomevelho,maiorhomem))
+print('Ao todo, são {} mulher com menos de 20 anos.'.format(totmulher20))
