c7dd5417438d012c1355ad5b5ea2c4bff4c8908e
Italo Pereira Vieira <mortybostinha@gmail.com>
2022-08-01 19:06:58 -0300

    realização do exercício do 60 pra baixo

--- cursoemvideo/desafios/desafio60.py
+++ cursoemvideo/desafios/desafio60.py
@@ -1,2 +1,17 @@
+from math import factorial
+from time import sleep
 n = int(input('Digite um número: '))
+c = n
+f = factorial(n)
+print('Calculando {}!'.format(n))
+sleep(0.5)
+print('Calculando.'.format(n))
+sleep(0.5)
+print('Calculando..'.format(n))
+sleep(0.5)
+print('CALCULADO!'.format(n))
 
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = {}'.format(f), end='')
+    c -= 1
