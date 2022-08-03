b4d1a166cc98c7bf4c32cc63c4721e38fc247c36
Italo Pereira Vieira <mortybostinha@gmail.com>
2022-08-03 18:01:31 -0300

    Correção do código no desafio 61

--- cursoemvideo/desafios/desafio61.py
+++ cursoemvideo/desafios/desafio61.py
@@ -1,7 +1,8 @@
 num = int(input('Digite um número inteiro: '))
 pa = int(input('Digite a razão aritmética: '))
-d = num + (10 - 1) * pa
-while not num > d:
+cont = 1
+while cont <= 10:
     num = num + pa
+    cont += 1
     print(num, end=' > ')
 print('FIM!')
--- cursoemvideo/desafios/desafio62.py
+++ cursoemvideo/desafios/desafio62.py
@@ -1,11 +1,19 @@
-from time import sleep
-c = 0
-
 num = int(input('Digite um número inteiro: '))
 pa = int(input('Digite a razão aritmética: '))
-d = num + (10 - 1) * pa
-while not num > d:
+cont = 1
+co = 0
+while cont <= 10:
     num = num + pa
+    cont += 1
+    co += 1
     print(num, end=' > ')
-    
-print('FIM!')
+print('Pausa')
+
+c = int(input('Quantos termos você quer mostrar a mais?: '))
+if c != 0:
+    while c != 0:
+        while cont <= 10 + c:
+            num = num + pa
+            cont +=1
+            co += 1
+            print(num, end=' > ')
