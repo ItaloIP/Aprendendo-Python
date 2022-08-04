46c9735a1cb52f9f39f084112b3c7aa6a750f64e
Italo Pereira Vieira <mortybostinha@gmail.com>
2022-08-04 05:48:25 -0300

    criei um programa para calcular a equação do 2 grau

---
+++ cursoemvideo/ideias/equaçãodo2.py
@@ -0,0 +1,20 @@
+from math import sqrt
+
+print('-'*25)
+print('\033[0;32;45mEquação do Segundo Grau\033[m')
+print('-'*25)
+
+a = int(input('Digite o valor do A: '))
+b = int(input('Digite o valor do B: '))
+c = int(input('Digite o valor do C: '))
+
+delta = b**2 - 4 * (a * c)
+
+menos = (-(b) - sqrt(delta)) / (2 * a)
+mais = (-(b) + sqrt(delta)) / (2 * a)
+
+print('=-='*15)
+print('X1 = {:.2f}'.format(mais))
+print('X2 = {:.2f}'.format(menos))
+print('=-='*15)
+
