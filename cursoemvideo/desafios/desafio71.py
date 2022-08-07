c6c5d58f59c5162148925108fb8ca5aeca951f9f
Italo Pereira Vieira <mortybostinha@gmail.com>
2022-08-07 12:07:16 -0300

    Finalizado todos os exercícios

--- cursoemvideo/desafios/desafio71.py
+++ cursoemvideo/desafios/desafio71.py
@@ -1,4 +1,41 @@
-#50, 20, 10, 1
-money = int(input('Quer sacar quanto?:  '))
-cin = money // 50
+from time import sleep
 
+print('-='*20)
+print('Banco Galatia')
+print('-='*20)
+sleep(1.0)
+print()
+
+money = int(input('Quer sacar quanto?:  '))
+cin = vin = dez = um = 0
+
+while True:
+    if money >= 50:
+        cin = money // 50
+        money -= (cin*50)
+        print(f'{cin} CÉDULAS DE R$50')
+        if money == 0:
+            break
+        
+    elif money >= 20:
+        vin = money // 20
+        money -= (vin*20)
+        print(f'{vin} CÉDULAS DE R$20')
+        if money == 0:
+            break
+
+    elif money >= 10:
+        dez = money // 10
+        money -= (dez*10)
+        print(f'{dez} CÉDULAS DE R$10')
+        if money == 0:
+            break
+
+    elif money >= 1:
+        um = money // 1
+        money -= (um*1)
+        print(f'{um} CÉDULAS DE R$1')
+        if money == 0:
+            break
+
+print('Volte sempre!')
--- cursoemvideo/pratica.py
+++ cursoemvideo/pratica.py
@@ -1,7 +1,2 @@
-print('\033[1;31;43mHello, World!\033[m')
-
-nome = 'José'
-idade = 33
-salario = 987.35
-
-print(f'O {nome} tem {idade} anos e recebe R${salario:.2f}')
+for c in range(0,10,3):
+    print(c)
