aa2284d5a721592d9815e5bf43b1753652642b60
Italo Pereira Vieira <mortybostinha@gmail.com>
2022-09-04 14:47:37 -0300

    Resolução do exercício 74

--- cursoemvideo/desafios/desafio74.py
+++ cursoemvideo/desafios/desafio74.py
@@ -1,41 +1,10 @@
 from random import randint
 
 
+numbers = (randint(0,10), randint(0,10), randint(0,10), randint(0,10), 
+            randint(0,10))
 
-um = randint(0,10)
-dois = randint(0,10)
-tres = randint(0,10)
-quatro = randint(0,10)
-cinco = randint(0,10)
-
-tupla = (um,dois,tres,quatro,cinco)
-print(tupla)
-
-
-
-maior = menor = um
-
-if um > maior:
-    maior = um
-elif dois > maior:
-    maior = dois
-elif tres > maior:
-    maior = tres
-elif quatro > maior:
-    maior = quatro
-elif cinco > maior:
-    maior = cinco
-
-
-
-if dois < menor:
-    menor = dois
-elif tres < menor:
-    menor = tres
-elif quatro < menor:
-    menor = quatro
-elif cinco < menor:
-    menor = cinco
-
-print(maior)
-print(menor)
+print(f'Os números sorteados foram {numbers}')
+ 
+print(f'O maior número é o {max(numbers)}')
+print(f'O menor número é o {min(numbers)}')
--- cursoemvideo/desafios/desafio75.py
+++ cursoemvideo/desafios/desafio75.py
@@ -6,18 +6,7 @@ pares = 0
 tupla = (number1,number2,number3,number4)
 tuplap = ''
 
-'''if number1 % 2 == 0:
-    tuplap += number1
-    print(f'O {number1} é PAR!')
-if number2 % 2 == 0:
-    tuplap += number2
-    print(f'O {number2} é PAR!')
-if number3 % 2 == 0:
-    tuplap += number3
-    print(f'O {number3} é PAR!')
-if number4 % 2 == 0:
-    tuplap += number4
-    print(f'O {number4} é PAR!')'''
+
 print('-'*20)
 print(f'Você digitou os valores {tupla}')
 print('O número NOVE apareceu', tupla.count(9), 'vezes!')
