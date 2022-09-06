db9700d2a9ad758a189f5a228bc7a5f728452b7e
Italo Pereira Vieira <mortybostinha@gmail.com>
2022-09-06 15:35:20 -0300

    Atualização do CDI

--- cursoemvideo/ideias/juroscdi.py
+++ cursoemvideo/ideias/juroscdi.py
@@ -2,7 +2,10 @@ n = float(input('Quanto de dinheiro você pretende investir?: '))
 cdi = float(input('Quanto que está proposto o CDI?: %'))
 a = int(input('Pretende deixar quantos anos investindo?: '))
 
-cd = (((cdi * 13.65) / 100) / 100) + 1
+#mude para o DIA do CDI que está realizando o processo
+dia = 13.65 
+
+cd = (((cdi * dia) / 100) / 100) + 1
 r = n * (cd ** a)
 r1 = r - n
 print('Você obterá R${:.2f} de lucro e ao todo, terá R${:.2f}'.format(r1, r))
