a1ad23224e0d28ef1c11a077b09a137c28fc302e
Italo Pereira Vieira <mortybostinha@gmail.com>
2022-10-01 19:24:02 -0300

    Tentando realizar ex 94

--- cursoemvideo/desafios/desafio94.py
+++ cursoemvideo/desafios/desafio94.py
@@ -1,29 +1,35 @@
-grupo = []
-pessoa = {}
-mulheres = []
+import enum
+
+
+grupo = list()
+pessoa = dict()
+mulheres = list()
+acmedia = list()
 sexos = ''
+
 vivo = soma = 0
 # --- #
 
 while True:
     pessoa['Nome'] = str(input('Nome: '))
     sexos = str(input('Sexo [M/F]: ')).strip().upper()
-    if sexos in 'MF':
+    if sexos == 'M':
         pessoa['Sexo'] = sexos 
         sexos = ''
-    if sexos == 'F':
+    elif sexos == 'F':
         mulheres.append(pessoa['Nome'])
-        grupo.append(mulheres[:])
-        mulheres.clear()
+        pessoa['Sexo'] = sexos 
+        sexos = ''
     else:
         while True:
             sexos = str(input('Tente novamente! [M/F]: ')).strip().upper()
-            if sexos in 'MF':
+            if sexos == 'M':
                 pessoa['Sexo'] = sexos 
-                grupo.append(mulheres[:])
-                mulheres.clear()
-            if sexos == 'F':
+                sexos = ''
+                break
+            elif sexos == 'F':
                 mulheres.append(pessoa['Nome'])
+                pessoa['Sexo'] = sexos 
                 sexos = ''
                 break
     ano = int(input('Idade: '))
@@ -50,12 +56,23 @@ while True:
             break
 
 media = soma / vivo
+for c, v in enumerate(grupo):
+    for a, b in enumerate(v.items()):
+        print(b['Idade'])
 
+    # if v[c][2] > media:
+    #     acmedia.append(v)
+# print(grupo)
+# print(acmedia)
 print('-='*30)
 
 print(f'Há {vivo} cadastradas.')
 print(f'A média de idade destas pessoas é {media}')
-print(f'As mulheres cadastradas foram: 'end= '')
-for c, v in 
-
-    
+print(f'As mulheres cadastradas foram: ',end='')
+for c, v in enumerate(mulheres):
+    print(f' {v},', end='')
+print(f'Pessoas que estão acima da média:',end='')
+print('')
+print('')
+for c, v in enumerate(acmedia):
+    print(f'nome = {acmedia[0]}; sexo = {acmedia[1]}; idade = {acmedia[2]}')
