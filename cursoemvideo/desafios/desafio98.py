95aa537027c7863e00c746d32f3d09b196da5e35
Italo Pereira Vieira <101375275+ItaloIP@users.noreply.github.com>
2022-10-06 16:25:49 -0300

    Desafio 98 + download youtube

--- cursoemvideo/desafios/desafio98.py
+++ cursoemvideo/desafios/desafio98.py
@@ -1,25 +1,31 @@
 from time import sleep
+
+
 def Contador(ini, fin, pa):
-    for b in range(ini, fin, pa):
-        print(b, end=' ')
-        sleep(0.6)
+    if pa < 0:
+        pa *= -1
+    print('-='*20)
+    print(f'Contagem de {ini} à {fin} de {pa} em {pa}')
+    if fin < ini:
+        fin = fin - 1
+        if pa > 0:
+            pa = -(pa)
+    else:
+        fin = fin + 1
+        if pa < 0:
+            pa = -(pa)
+
+    for fin in range(ini, fin, pa):
+        print(fin, end=' ')
+        sleep(0.3)
+    print('fim!')
+    print('-='*20)
 
 
 # Programa Principal
-print('Contagem de 0 à 10 de 1 em 1')
-for c in range(0, 11):
-    print(c, end=' ')
-    sleep(0.3)
-print() # linha vazia
-print('-='*20)
-print('Contagem de 10 a 0 de 2 em 2')
-for v in range(10, -1, -2):
-    print(v, end=' ')
-    sleep(0.3)
-print() # linha vazia
-print('-='*20)
+Contador(0,10,1)
+Contador(10,0,2)
 print('Sua vez de fazer uma progressão aritmética')
-
 a = int(input('Início: '))
 b = int(input('Final: '))
 while True:
@@ -28,10 +34,4 @@ while True:
         break
     else:
         print('O passo não pode ser menor que 0!')
-if b < a:
-    c = -(c)
-    b = b - 1
-else:
-    b = b + 1
-
 Contador(a, b, c)
--- cursoemvideo/ideias/mp3youtube.py
+++ cursoemvideo/ideias/mp3youtube.py
@@ -2,6 +2,29 @@ from pytube import YouTube
 import moviepy.editor as mp
 import re
 import os
+def MP3(url):
+    path = str('Download')
+    yt = YouTube(url)
+    ys = yt.streams.filter(only_audio=True).first().download(path)
+    print('-='*20)
+    print('Baixado!')
+    print('-='*20)
+    print('Convertendo para MP3')
+    print('-='*20)
+    for file in os.listdir(path):
+        if re.search('mp4', file):
+            mp4_path = os.path.join(path, file)
+            mp3_path = os.path.join(path, os.path.splitext(file)[0] +' .mp3')
+            new_file = mp.AudioFileClip(mp4_path)
+            new_file.write_audiofile(mp3_path)
+            os.remove(mp4_path)
+def MP4(url):
+    path = str('Download')
+    yt = YouTube(url)
+    ys = yt.streams.filter().first().download(path)
+    print('-='*20)
+    print('Baixado!')
+    
 #Instale o PYTUBE e MOVIEPY
 while True:
 
@@ -10,23 +33,12 @@ while True:
     ( 1 ) MP4
     :  '''))
     if choice == 0:
-        url = str(input('Link: '))
-        path = str('C:\Users\italo\Downloads')
-        yt = YouTube(url)
-        ys = yt.streams.filter(only_audio=True).first().download(path)
-        print('-='*20)
-        print('Baixado!')
-        print('-='*20)
-        print('Convertendo para MP3')
-        print('-='*20)
-        for file in os.listdir(path):
-            if re.search('mp4', file):
-                mp4_path = os.path.join(path, file)
-                mp3_path = os.path.join(path, os.path.splitext(file)[0] +' .mp3')
-                new_file = mp.AudioFileClip(mp4_path)
-                new_file.write_audiofile(mp3_path)
-                os.remove(mp4_path)
+        linkk = str(input('Link (only youtube!): '))
+        MP3(linkk)
         break
+    elif choice == 1:
+        linkk = str(input('Link (only youtube!): '))
+        MP4(linkk)
     else:
         print('Erro!')
 
