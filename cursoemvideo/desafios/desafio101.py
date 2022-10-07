56e6bc3b54e8020e3c818990aaf428ee22be1957
Italo Pereira Vieira <101375275+ItaloIP@users.noreply.github.com>
2022-10-07 16:43:06 -0300

    tentando realizar exercício 101

---
+++ cursoemvideo/desafios/desafio101.py
@@ -0,0 +1,14 @@
+from datetime import date, datetime
+year = date.today().year
+print(year)
+def Votar(idade):
+    s = year - ano
+    if s <= 15:
+        print('Não vota.')
+        return s
+
+
+
+#Programa Principal
+ano = int(input('Ano de nascimento: '))
+print(ano)
