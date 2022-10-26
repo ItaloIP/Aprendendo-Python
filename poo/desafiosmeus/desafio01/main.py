7a761804a6f32bc4e8e23d62fa360434226ccdad
Italo Pereira Vieira <101375275+ItaloIP@users.noreply.github.com>
2022-10-26 20:26:56 -0300

    atualização no sistema

--- poo/desafiosmeus/desafio01/main.py
+++ poo/desafiosmeus/desafio01/main.py
@@ -1,7 +1,6 @@
 import classes
-produtos = []
 #Produtos
-
+produtos = []
 produtos.append(classes.Produtos('Leite Condensado'))
 produtos.append(classes.Produtos('Arroz'))
 
@@ -12,15 +11,37 @@ while True:
     choice = int(input('''
 
 [ 0 ] Repor estoque
-[ 1 ] Novo preço 
-[ 2 ] Comprar
+[ 1 ] Adicionar novo item
+[ 2 ] Novo preço 
+[ 3 ] Comprar
 SELECT: '''))
     if choice == 0:
-        print(f'[ 0 ]{produtos[0].nome} : QUANT {produtos[0].unidades}')
-        print(f'[ 1 ]{produtos[1].nome} : QUANT {produtos[1].unidades}')
+        for c, v in enumerate(produtos):
+            print(f'({c})R${produtos[c].preço} {produtos[c].nome} : QUANT {produtos[c].unidades}')
+
         produto = int(input('Qual produto quer repor?: '))
-        if produto == 0:
-            produtos[0].Reposicao(int(input('Número: ')))
-        elif produto == 1:
-            produtos[1].Reposicao(int(input('Número: ')))
-        
+        produtos[produto].Reposicao(int(input('Quantidade à mais: ')))
+
+    elif choice == 1:
+        produtos.append(classes.Produtos(str(input('Nome do produto: '))))
+
+    elif choice == 2:
+        for c, v in enumerate(produtos):
+            print(f'({c})R${produtos[c].preço} {produtos[c].nome}')
+
+        produto = int(input('Qual produto quer alterar o preço?: '))
+        produtos[produto].NewPrice(float(input(f'Novo preço do(a) {produtos[produto].nome}: R$')))
+
+    elif choice == 3:
+        for c, v in enumerate(produtos):
+            if produtos[c].unidades > 0 and produtos[c].preço > 0:
+                print(f'({c}){produtos[c].nome} por R${produtos[c].preço}')
+                comprar = int(input('Qual item deseja comprar?: '))
+                uni = int(input('Quantas quantidades?: '))
+                try:
+                    produtos[comprar].Comprar(uni)
+                except:
+                    print('Algo deu errado!')
+                else: 
+                    print('Compra feita!')
+                
